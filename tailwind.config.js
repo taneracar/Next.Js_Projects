d08225219fee35be998ce7202ad999aff5e0d810
Taner Acar <151092957+taneracar@users.noreply.github.com>
2024-05-22 15:42:23 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      satoshi: ["Satoshi", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      "primary-orange": "#FF5722",
+    },
+  },
+};
+export const plugins = [];
